8d8d4f7b42f8ee691d68c37f008c0869a98aaf74
GAP <96944057+kodegede@users.noreply.github.com>
2022-10-24 19:16:55 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,16 +2,15 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 @app.get("/add")
-async def addition(x: int, y: int):
+async def addition(x:int, y:int):
     return {
-        "data": x+y,
-        "message": "success"
+        "data": x + y,
+        "message": "success",
     }
 
 @app.get("/ready")
